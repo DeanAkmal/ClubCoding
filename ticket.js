d5e5999d4d5de45552e3a3172b864e599d59f591
DeanAkmal <118705266+DeanAkmal@users.noreply.github.com>
2025-12-07 17:47:39 +0700

    feat: money logic

--- ticket.js
+++ ticket.js
@@ -1,9 +1,20 @@
 const input = require('readline-sync');
 
 const nama = input.question();
-const umur = input.question();
+const umur = Number(input.question());
 
 
 if (umur < 21){
     console.log(`Maaf ${nama}, umur kamu kurang ` + (21 - umur) + " tahun lagi")
 }
+
+if (umur >= 21){
+    const uang = Number(input.question());
+    if(uang < 500000){
+        console.log(`Maaf ${nama}, uang kamu cuma ` + (uang / 1000) + " ribu," + " datang lagi lain kali");
+    }
+    else if (uang > 500000) {
+        console.log(`Selamat datang ${nama}`);
+    }
+}
+
